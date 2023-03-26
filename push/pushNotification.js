b72e127863836e808b7a847edab1bcc30db8f458
desmondwa <59489905+desmondwa@users.noreply.github.com>
2023-03-26 01:09:33 -0400

    Update pushNotification.js
    i tried

--- push/pushNotification.js
+++ push/pushNotification.js
@@ -1,12 +1,21 @@
 const PushAPI = require("@pushprotocol/restapi");
 
+<<<<<<< chat_page
+export const fetchNotifs = async() => {
+=======
 const fetchNotifs = async() => {
+>>>>>>> main
     const notifications = await PushAPI.user.getFeeds({
         user: 'eip155:42:0xD8634C39BBFd4033c0d3289C4515275102423681', // user address in CAIP-10
         env: 'staging'
     });
 
+<<<<<<< chat_page
+    return(`Notifications: \n', ${notifications}`);
+}
+=======
     console.log('Notifications: \n', notifications);
 }
 
 fetchNotifs();
+>>>>>>> main
